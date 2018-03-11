10222d655062d564be7393cc4ec290fd1254e9bd
DorWeid <dorprozor@gmail.com>
2018-03-11 11:08:48 +0200

    valate

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,10 @@
-import User from '../models/user.model';
-import List from '../models/list.model';
-import { validate } from '../helpers/utils';
+import User from "../models/user.model";
+import List from "../models/list.model";
+import { validate } from "../helpers/utils";
 
 function load(req, res, next, username) {
   User.get(username)
-    .then((user) => {
+    .then(user => {
       req.user = user; // eslint-disable-line no-param-reassign
       return next();
     })
@@ -16,14 +16,11 @@ function get(req, res) {
 }
 
 function create(req, res, next) {
-  const userData = valate(req.body,
-    ['username', 'password']
-  );
-  const user = new User(
-    userData
-  );
+  const userData = validate(req.body, ["username", "password"]);
+  const user = new User(userData);
 
-  user.save()
+  user
+    .save()
     .then(savedUser => res.json(savedUser))
     .catch(e => next(e));
 }
@@ -31,7 +28,8 @@ function create(req, res, next) {
 function update(req, res, next) {
   const { user } = req;
 
-  user.save(req.body)
+  user
+    .save(req.body)
     .then(savedUser => res.json(savedUser))
     .catch(e => next(e));
 }
@@ -45,7 +43,8 @@ function list(req, res, next) {
 
 function remove(req, res, next) {
   const { user } = req;
-  user.remove()
+  user
+    .remove()
     .then(deletedUser => res.json(deletedUser))
     .catch(e => next(e));
 }
@@ -53,8 +52,8 @@ function remove(req, res, next) {
 function getCreatedLists(req, res, next) {
   const { user } = req;
   List.byCreators(user.username)
-  .then(lists => res.json(lists))
-  .catch(e => next(e));
+    .then(lists => res.json(lists))
+    .catch(e => next(e));
 }
 
 export default { load, get, create, update, list, remove, getCreatedLists };
