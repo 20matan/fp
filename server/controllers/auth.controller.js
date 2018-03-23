30d2094ef3d3bf22699a5f9cbe3e5bf81f6df60c
Matan <20matan@gmail.com>
2018-03-24 00:32:18 +0300

    lint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,42 +1,42 @@
-import User from "../models/user.model";
-import { facebookAuth } from "../helpers/utils";
-import { generateToken } from "../helpers/auth";
+import User from '../models/user.model'
+import { facebookAuth } from '../helpers/utils'
+import { generateToken } from '../helpers/auth'
 
 function login(req, res, next) {
   if (!req.body.access_token) {
-    next(new Error("No access_token was sent in the body"));
-    return;
+    next(new Error('No access_token was sent in the body'))
+    return
   }
 
   facebookAuth(req.body.access_token)
     .then(({ data }) => {
-      console.log("res", data);
+      console.log('res', data)
       if (data.error) {
-        next(data.error);
-        return;
+        next(data.error)
+        return
       }
-      console.log("no error in facebook auth, moving on");
+      console.log('no error in facebook auth, moving on')
 
       User.findOrCreate(
         data.id,
         Object.assign({}, data, { username: data.name })
-      ).then(creationRes => {
-        console.log("creationRes", creationRes);
-        const { user } = creationRes;
-        console.log("user from creation", user);
+      ).then((creationRes) => {
+        console.log('creationRes', creationRes)
+        const { user } = creationRes
+        console.log('user from creation', user)
 
-        const token = generateToken(user);
+        const token = generateToken(user)
         const options = {
           maxAge: 864 * 10000000, // 100 days
           httpOnly: true // The cookie only accessible by the web server
-        };
-        res.cookie("access-token", token, options);
+        }
+        res.cookie('access-token', token, options)
         // TODO: return jwt
-        res.send({ succcess: true, token, user });
-        return;
-      });
+        res.send({ succcess: true, token, user })
+        return
+      })
     })
-    .catch(next);
+    .catch(next)
 }
 
-export default { login };
+export default { login }
